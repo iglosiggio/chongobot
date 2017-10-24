b030af2549dad5cc56f949a8f7fa5874685ab011
Ignacio Losiggio <iglosiggio@gmail.com>
2017-10-24 00:52:33 -0300

    Agregada alarma (no testeada)

--- index.js
+++ index.js
@@ -78,6 +78,29 @@ bot.onText(/^\/editarevento_([0-9]+) ([^ ]+) (.+)$/, comando((msg, [texto, event
   bot.sendMessage(chatId, `Modificado! Si te equivotaste de vuelta usá /editarevento_${eventoId}`);
 }));
 
+/* TODO: modularizar esto junto al resto del código */
+(function() {
+  const ahora = moment();
+  const horario_alarma = '10:00';
+  const grupos_alarma = [-235836218];
+  const formato_alarma = ['hh:dd', 'hh:dd:ss'];
+  const alarma = moment(horario_alarma, formato_alarma, true);
+
+  if(ahora > alarma) alarma.add(1, 'days');
+
+  setTimeout(() => {
+    setInterval(() => {
+      const fecha = moment();
+      const eventos_texto = describir_fecha(materias.concat(eventos), fecha, config);
+
+      if(!texto) return;
+
+      grupos_alarma.forEach(grupo =>
+        bot.sendMessage(grupo, eventos_texto, {parse_mode: 'markdown'}));
+    }, 24*60*60*1000);
+  }, alarma - ahora);
+})();
+
 function describir_fecha(eventos, fecha, config) {
   const eventos_hoy = eventos_en_calendario(eventos, fecha);
 
