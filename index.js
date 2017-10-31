fc10055f17e875dbd09a2414bea8159e880c1cbf
Ignacio Losiggio <iglosiggio@gmail.com>
2017-10-31 00:00:15 -0300

    Loguear errores, retornar promesas, usar las promesas del sistema, cambiar algunas respuestas, etc (debería hacer commits como la gente)

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const moment = require('moment');
 const fs = require('fs');
 const crypto = require('crypto');
 
+/* Use internal promises */
+TelegramBot.promise = Promise;
+
 const bot = new TelegramBot(token, {polling: true});
 
 bot.onText(/^\/materias ?(-?[0-9]+)?/, comando((msg, [texto, días = 0]) => {
@@ -19,15 +22,19 @@ bot.onText(/^\/materias ?(-?[0-9]+)?/, comando((msg, [texto, días = 0]) => {
   const respuesta = describir_fecha(materias, fecha, config);
   const sin_materias = `${días == 0 ? 'Hoy' : `El ${fecha.format('DD/MM')}`} no cursás nada. ¡Descansá!`;
 
-  bot.sendMessage(chatId, respuesta || sin_materias, {parse_mode: 'markdown'});
+  return bot.sendMessage(chatId, respuesta || sin_materias, {parse_mode: 'markdown'})
+            .catch(log_catch('materias'));
 }));
 
 bot.onText(/^\/(sugerencia|recordar|sugerir).*/, comando(msg => {
   const chatId = msg.chat.id;
   const msgId = msg.message_id;
 
-  bot.forwardMessage(dueño, msg.chat.id, msgId);
-  bot.sendMessage(chatId, 'Gracias! Ahí lo molesto a nacho.');
+  const fw = bot.forwardMessage(dueño, msg.chat.id, msgId)
+                .catch(log_catch('sugerir_forward');
+  const respuesta = bot.sendMessage(chatId, 'Gracias! Ahí lo molesto a nacho.')
+                       .catch(log_catch('sugerir_mensaje'));
+  return Promise.all([fw, respuesta]);
 }));
 
 bot.onText(/^\/(calendario|crono|cronograma|eventos) ?(-?[0-9]+)?/, comando((msg, [texto, _, días = 0]) => {
@@ -37,7 +44,8 @@ bot.onText(/^\/(calendario|crono|cronograma|eventos) ?(-?[0-9]+)?/, comando((msg
   const respuesta = describir_fecha(materias.concat(eventos), fecha, config);
   const sin_eventos = `${días == 0 ? 'Hoy' : `El ${fecha.format('DD/MM')}`} no hay nada para hacer, que aburrido :(`;
 
-  bot.sendMessage(chatId, respuesta || sin_eventos, {parse_mode: 'markdown'});
+  return bot.sendMessage(chatId, respuesta || sin_eventos, {parse_mode: 'markdown'})
+            .catch(log_catch('calendario'));
 }));
 
 bot.onText(/^\/agendar ([^ ]+) (.+)$/, comando((msg, [texto, fecha, título]) => {
@@ -45,21 +53,23 @@ bot.onText(/^\/agendar ([^ ]+) (.+)$/, comando((msg, [texto, fecha, título]) =>
   const evento = crear_evento(fecha, título);
 
   if(evento instanceof Error) {
-    bot.sendMessage(chatId, evento.message);
-    return;
+    return bot.sendMessage(chatId, evento.message)
+              .catch(log_catch('agendar_error'));
   }
 
   const evento_texto = describir_evento(evento, null, config);
   const eventoId = agregar_evento(evento, eventos);
 
-  bot.sendMessage(chatId, `${evento_texto}\n\nAgendado! En caso de error usá /editarevento\\_${eventoId}`, {parse_mode: 'markdown'});
+  return bot.sendMessage(chatId, `${evento_texto}\n\nAgendado! En caso de error usá /editarevento\\_${eventoId}`, {parse_mode: 'markdown'})
+            .catch(log_catch('agendar'));
 }));
 
 bot.onText(/^\/(evento|verevento)_([0-9]+)/, comando((msg, [texto, verbo, eventoId]) => {
   const chatId = msg.chat.id;
   const evento = eventos[eventoId];
 
-  bot.sendMessage(chatId, describir_evento(evento, null, config), {parse_mode: 'markdown'});
+  return bot.sendMessage(chatId, describir_evento(evento, null, config), {parse_mode: 'markdown'})
+            .catch(log_catch('verevento'));
 }));
 
 bot.onText(/^\/editarevento_([0-9]+) ([^ ]+) (.+)$/, comando((msg, [texto, eventoId, fecha, título]) => {
@@ -68,8 +78,8 @@ bot.onText(/^\/editarevento_([0-9]+) ([^ ]+) (.+)$/, comando((msg, [texto, event
   const nuevoEvento = crear_evento(fecha, título);
 
   if(evento instanceof Error) {
-    bot.sendMessage(chatId, evento.message);
-    return;
+    return bot.sendMessage(chatId, evento.message)
+              .catch('editarevento_error');
   }
 
   for(dato in nuevoEvento) {
@@ -80,7 +90,8 @@ bot.onText(/^\/editarevento_([0-9]+) ([^ ]+) (.+)$/, comando((msg, [texto, event
 
   guardar_eventos(eventos);
 
-  bot.sendMessage(chatId, `${evento_texto}\nModificado! Si te equivotaste de vuelta usá /editarevento\\_${eventoId}`, {parse_mode: 'markdown'});
+  return bot.sendMessage(chatId, `${evento_texto}\nModificado! Si te equivocaste de vuelta usá /editarevento\\_${eventoId}`, {parse_mode: 'markdown'})
+            .catch(log_catch('editarevento'));
 }));
 
 bot.onText(/^\/(pregunta|preguntar) (.+)/, comando((msg, [texto, verbo, pregunta]) => {
@@ -98,23 +109,24 @@ bot.onText(/^\/(pregunta|preguntar) (.+)/, comando((msg, [texto, verbo, pregunta
   const respuestas = {
     "0": "Obvio!",
     "1": "Yup.",
-    "2": "Mmm...\nmmm...\nmmmmmm....\n\nehm, ponele que sí, ponele (?",
+    "2": "La probabilidad de eso es aproximadamente de 3720 a 1",
     "3": "Por su pollo",
     "4": "Sí.",
     "5": "Yo creo que sí Kent",
-    "6": "Of course",
+    "6": "Q c sho",
     "7": "OBVIO QUE SÍ; yo si estuviera en tu lugar no hubiera dudado como un gil",
     "8": "Jamás",
     "9": "No.",
     "a": "Ni en pedo",
-    "b": "Ehhh, yo que vos no",
-    "c": "NO\n\n(es una trampa)",
-    "d": "NEIN",
+    "b": "Estaría copado, no?",
+    "c": "Interesante,\ninteresante.\n\n\nNo.",
+    "d": "Quiero creer que no",
     "e": "Lo dudo mucho",
     "f": "No lo creo",
   };
 
-  bot.sendMessage(chatId, respuestas[hash_index]);
+  return bot.sendMessage(chatId, respuestas[hash_index])
+            .catch(log_catch('preguntar'));
 }));
 
 /* TODO: modularizar esto junto al resto del código */
@@ -130,8 +142,11 @@ bot.onText(/^\/(pregunta|preguntar) (.+)/, comando((msg, [texto, verbo, pregunta
 
     if(!eventos_texto) return;
 
-    grupos.forEach(grupo =>
-      bot.sendMessage(grupo, eventos_texto, {parse_mode: 'markdown'}));
+    const mensajes = grupos.map(grupo =>
+      bot.sendMessage(grupo, eventos_texto, {parse_mode: 'markdown'}))
+         .catch(log_catch(`alarma_${grupo}`));
+
+    return Promise.all(mensajes);
   }
 
   if(ahora > alarma) alarma.add(1, 'days');
@@ -215,5 +230,11 @@ function comando(cmd) {
       return;
     }
     return cmd(msg, ...args);
+  }
+}
+
+function log_catch(lugar) {
+  return function(error) {
+    console.error(``[ERROR:${lugar}]`, error.message, error.code)
   }
 }
